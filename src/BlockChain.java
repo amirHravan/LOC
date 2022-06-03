9e0fc30f7891d206a488c30e3d9b4042ae31d586
Amir-H-ravan <amir.h.rnn@gmail.com>
2022-06-03 05:03:07 +0430

    progress through question2

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -32,14 +32,14 @@ enum Regexes {
 class User {
     private final User introducer;
     private String username;
-    private Level floor;
+    private Level level;
     private int deposit;
     private int position;
 
     public User(User introducer, String username, Level floor, int deposit, int position) {
         this.introducer = introducer;
         this.username = username;
-        this.floor = floor;
+        this.level = floor;
         this.deposit = deposit;
         this.position = position;
     }
@@ -64,12 +64,12 @@ class User {
         this.username = username;
     }
 
-    public Level getFloor() {
-        return floor;
+    public Level getLevel() {
+        return level;
     }
 
-    public void setFloor(Level floor) {
-        this.floor = floor;
+    public void setLevel(Level level) {
+        this.level = level;
     }
 
     public int getDeposit() {
@@ -78,6 +78,10 @@ class User {
 
     public void setDeposit(int deposit) {
         this.deposit = deposit;
+    }
+
+    public boolean isFriend(User user,int peopleInLLevel){
+        return this.getPosition() == (user.getPosition() + 1) % peopleInLLevel || this.getPosition() == (user.getPosition() + peopleInLLevel - 1) % peopleInLLevel;
     }
 }
 
@@ -121,21 +125,116 @@ class Mahdiz {
 
 class FinancialCircle {
     private final ArrayList<User> users;
+    private final ArrayList<Level> levels;
     private Mahdiz mahdiz;
 
     public FinancialCircle() {
         this.users = new ArrayList<>();
+        this.levels = new ArrayList<>();
+
+    }
+
+    public ArrayList<Level> getLevels() {
+        return levels;
+    }
+
+    public Mahdiz getMahdiz() {
+        return mahdiz;
     }
 
     public ArrayList<User> getUsers() {
         return users;
+    }
+
+    public void setMahdiz(Mahdiz mahdiz) {
+        this.mahdiz = mahdiz;
     }
 }
 
 class Controller {
     private final FinancialCircle financialCircle = new FinancialCircle();
 
+    private User getUserByName(String username){
+        for (User user : financialCircle.getUsers()) {
+            if (user.getUsername().equals(username))
+                return user;
+        }
+        return null;
+
+    }
+
+
+    public String createTable(String username, int deposit) {
+
+    }
+
+    public String joinIndependent(String username, int deposit) {
+
+    }
+
+
+    public String joinWithInvitation(String oldUsername, String newUsername, int deposit) {
+
+    }
+
+    public int getNumberOfUsers() {
+        return financialCircle.getUsers().size();
+    }
+
+    public int getNumberOfLevels() {
+        return financialCircle.getLevels().size();
+    }
+
+    public ArrayList<User> getUsersInSameLevel(int levelNumber) {
+        ArrayList<User> users = new ArrayList<>();
+
+        for (User user : financialCircle.getUsers()) {
+            if (user.getLevel().getNumber() == levelNumber)
+                users.add(user);
+        }
+
+        return users;
+    }
+
+    public int getMahdizDeposit() {
+        return financialCircle.getMahdiz().getDeposit();
+    }
+    public ArrayList<User> getUserFriends(String username) {
+        User targetUser = getUserByName(username);
+        ArrayList<User> userFriends = new ArrayList<>();
+
+        for (User user : financialCircle.getUsers()) {
+            if (user.getLevel().getNumber() == targetUser.getLevel().getNumber()){
+                if (targetUser.isFriend(user,financialCircle.getUsers().size()))
+                    userFriends.add(user);
+            }
+        }
+        return userFriends;
+    }
+
+    public String getUserIntroducer(String username) {
+        User user = getUserByName(username);
+        if (user == null)
+            return "1";
+        if (user.getIntroducer() == null)
+            return "2";
+
+        return "3";
+    }
+
+    public String getUserDeposit(String username) {
+        User user = getUserByName(username);
+        if (user == null)
+            return "1";
+
+        return String.valueOf(user.getDeposit());
+    }
+}
+
+class Menu {
     public void run() {
+        Controller controller = new Controller();
+
         String input;
         Scanner scanner = new Scanner(System.in);
         Matcher matcher;
@@ -143,30 +242,55 @@ class Controller {
         while (true) {
             input = scanner.nextLine();
 
-            if (Regexes.getMatcher(input, Regexes.CREATE_TABLE).matches()) {
+            if ((matcher = Regexes.getMatcher(input, Regexes.CREATE_TABLE)).matches()) {
+                System.out.println(controller.createTable(matcher.group(1), Integer.parseInt(matcher.group(2))));
 
-            } else if (Regexes.getMatcher(input, Regexes.JOIN_INDEPENDENT).matches()) {
+            } else if ((matcher = Regexes.getMatcher(input, Regexes.JOIN_INDEPENDENT)).matches()) {
+                System.out.println(controller.joinIndependent(matcher.group(1), Integer.parseInt(matcher.group(2))));
 
-            } else if (Regexes.getMatcher(input, Regexes.JOIN_WITH_INVITATION).matches()) {
+            } else if ((matcher = Regexes.getMatcher(input, Regexes.JOIN_WITH_INVITATION)).matches()) {
+                System.out.println(controller.joinWithInvitation(matcher.group(1), matcher.group(2), Integer.parseInt(matcher.group(3))));
 
-            } else if (Regexes.getMatcher(input, Regexes.NUMBER_OF_USERS).matches()) {
+            } else if ((matcher = Regexes.getMatcher(input, Regexes.NUMBER_OF_USERS)).matches()) {
+                System.out.println(controller.getNumberOfUsers());
 
-            } else if (Regexes.getMatcher(input, Regexes.NUMBER_OF_LEVELS).matches()) {
+            } else if ((matcher = Regexes.getMatcher(input, Regexes.NUMBER_OF_LEVELS)).matches()) {
+                System.out.println(controller.getNumberOfLevels());
 
-            } else if (Regexes.getMatcher(input, Regexes.NUMBER_OF_USERS_IN_LEVEL).matches()) {
+            } else if ((matcher = Regexes.getMatcher(input, Regexes.USER_CREDITS)).matches()) {
+                System.out.println(controller.getUserDeposit(matcher.group(1)));
 
-            } else if (Regexes.getMatcher(input, Regexes.PROFIT_UNTIL_NOW).matches()) {
+            } else if ((matcher = Regexes.getMatcher(input, Regexes.NUMBER_OF_USERS_IN_LEVEL)).matches()) {
+                System.out.println(controller.getUsersInSameLevel(Integer.parseInt(matcher.group(1))).size());
 
-            } else if (Regexes.getMatcher(input, Regexes.SAME_LEVEL_USERS).matches()) {
+            } else if ((matcher = Regexes.getMatcher(input, Regexes.PROFIT_UNTIL_NOW)).matches()) {
+                System.out.println(controller.getMahdizDeposit());
 
-            } else if (Regexes.getMatcher(input, Regexes.USER_FRIENDS).matches()) {
+            } else if ((matcher = Regexes.getMatcher(input, Regexes.SAME_LEVEL_USERS)).matches()) {
+                if (controller.getUsersInSameLevel(Integer.parseInt(matcher.group(1))).size() == 0)
+                    System.out.println("");
 
-            } else if (Regexes.getMatcher(input, Regexes.USER_INTRODUCER).matches()) {
+                for (User user : controller.getUsersInSameLevel(Integer.parseInt(matcher.group(1)))) {
+                    System.out.println(user);
+                }
+
+            } else if ((matcher = Regexes.getMatcher(input, Regexes.USER_FRIENDS)).matches()) {
+                if (controller.getUserFriends(matcher.group(1)).size() == 0)
+                    System.out.println("");
+
+                for (User userFriend : controller.getUserFriends(matcher.group(1))) {
+                    System.out.println(userFriend);
+                }
+
+            } else if ((matcher = Regexes.getMatcher(input, Regexes.USER_INTRODUCER)).matches()) {
+                System.out.println(controller.getUserIntroducer(matcher.group(1)));
 
             } else if (Regexes.getMatcher(input, Regexes.END).matches()) {
                 break;
+
             } else {
                 System.out.println("invalid command!");
+
             }
         }
     }
@@ -175,7 +299,7 @@ class Controller {
 public class BlockChain {
 
     public static void main(String[] args) {
-        Controller controller = new Controller();
+        Menu controller = new Menu();
         controller.run();
     }
 }
